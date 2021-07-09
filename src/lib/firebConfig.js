c5506db04cc8eb2e48e793dfa94e758da1f8087c
carlamorales <carlamoralespacheco@gmail.com>
2021-07-09 17:56:34 -0400

    el objeto firebaseConfig se ubica en su propio archivo y se lo invoca desde el archivo main

---
+++ src/lib/firebConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDJ7ZYRnV20S3KLQo-jERy9HDR3d0V3n2Q',
+  authDomain: 'proyectodeprueba123.firebaseapp.com',
+  projectId: 'proyectodeprueba123',
+  storageBucket: 'proyectodeprueba123.appspot.com',
+  messagingSenderId: '446930050033',
+  appId: '1:446930050033:web:af0c65ef5ea96f3b1c4250',
+};
--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
+import { firebaseConfig } from './lib/firebConfig.js';
 import { signUpFunction } from './lib/signUp.js';
 import { signInFunction } from './lib/signIn.js';
 
+firebase.initializeApp(firebaseConfig);
 signUpFunction();
 signInFunction();
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDJ7ZYRnV20S3KLQo-jERy9HDR3d0V3n2Q',
-  authDomain: 'proyectodeprueba123.firebaseapp.com',
-  projectId: 'proyectodeprueba123',
-  storageBucket: 'proyectodeprueba123.appspot.com',
-  messagingSenderId: '446930050033',
-  appId: '1:446930050033:web:af0c65ef5ea96f3b1c4250',
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
