66d58fc617bb7e5412ceb06a320b11fb14f7dade
carlamorales <carlamoralespacheco@gmail.com>
2021-07-14 18:47:15 -0400

    se corrige vista intro

--- src/lib/intro.js
+++ src/lib/intro.js
@@ -1,11 +1,21 @@
 export const introView = () => {
   const containerIntro = document.createElement('div');
-  const buttonIntro = document.createElement('button');
-  buttonIntro.setAttribute('id', 'btnIntro');
+  const divGif = document.createElement('div');
+  divGif.setAttribute('id', 'divGif');
+  containerIntro.appendChild(divGif);
+  const divTextIntro = document.createElement('div');
+  divTextIntro.setAttribute('id', 'divTextIntro');
+  divTextIntro.innerHTML = '<strong>Conecta con tejedoras y tejedores de todo el mundo</strong>';
+  containerIntro.appendChild(divTextIntro);
+  const divButtonIntro = document.createElement('div');
+  divButtonIntro.setAttribute('id', 'divButtonIntro');
+  const buttonIntro= document.createElement('button');
+  buttonIntro.setAttribute('id', 'buttonIntro');
   buttonIntro.innerHTML = 'Comienza';
   buttonIntro.addEventListener('click', () => {
     window.location.href = '#/signup';
-  });
-  containerIntro.appendChild(buttonIntro);
+  })
+  divButtonIntro.appendChild(buttonIntro);
+  containerIntro.appendChild(divButtonIntro);
   return containerIntro;
 }
