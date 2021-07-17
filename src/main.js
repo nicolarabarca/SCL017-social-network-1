707405c85322869d677faf9f412df62f4d6dacd2
carlamorales <carlamoralespacheco@gmail.com>
2021-07-17 18:32:18 -0400

    se agrega observador de firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { firebaseConfig } from './lib/firebConfig.js';
+import { observer } from './observerFB.js';
 import { init } from './lib/router.js';
 
 firebase.initializeApp(firebaseConfig);
+observer();
 window.addEventListener('load', init);
