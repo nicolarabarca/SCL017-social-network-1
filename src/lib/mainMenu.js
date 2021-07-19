5519e410ddd800bd10e7a97a4f4b7ef4fe77d887
carlamorales <carlamoralespacheco@gmail.com>
2021-07-19 13:01:09 -0400

    las imágenes se vuelven cliqueables

--- src/lib/mainMenu.js
+++ src/lib/mainMenu.js
@@ -1,20 +1,21 @@
 export const mainMenuView = () => {
   const containerMainMenu = document.createElement('div');
   containerMainMenu.setAttribute('id', 'containerMainMenu');
-  
   const headerMainMenu = document.createElement('header');
   headerMainMenu.setAttribute('id', 'headerMainMenu');
   const titleMainMenu = document.createElement('h1');
   titleMainMenu.innerHTML = 'Tejer & Punto';
   headerMainMenu.appendChild(titleMainMenu);
   containerMainMenu.appendChild(headerMainMenu);
-
   const mainMenuOptions = document.createElement('main');
   const boxKnittingSticks = document.createElement('div');
   boxKnittingSticks.setAttribute('id', 'boxKnittingSticks');
+  const boxKnittingSticksAnchor = document.createElement('a');
+  boxKnittingSticksAnchor.setAttribute('href', '') //acá ponemos la ruta a la que nos lleva la imagen.
   const boxSticksImg = document.createElement('img');
   boxSticksImg.setAttribute('id', 'boxSticksImg');
-  boxKnittingSticks.appendChild(boxSticksImg);
+  boxKnittingSticksAnchor.appendChild(boxSticksImg);
+  boxKnittingSticks.appendChild(boxKnittingSticksAnchor);
   const boxSticksText = document.createElement('p');
   boxSticksText.setAttribute('id', 'boxSticksText');
   boxSticksText.innerHTML = 'Palillos';
@@ -22,20 +23,21 @@ export const mainMenuView = () => {
   mainMenuOptions.appendChild(boxKnittingSticks);
   const boxCrochet = document.createElement('div');
   boxCrochet.setAttribute('id', 'boxCrochet');
+  const boxCrochetAnchor = document.createElement('a');
+  boxCrochetAnchor.setAttribute('href', '') // acá ponemos la ruta a la que nos lleva la imagen.
   const boxCrochetImg = document.createElement('img');
   boxCrochetImg.setAttribute('id', 'boxCrochetImg');
-  boxCrochet.appendChild(boxCrochetImg);
+  boxCrochetAnchor.appendChild(boxCrochetImg);
+  boxCrochet.appendChild(boxCrochetAnchor);
   const boxCrochetText = document.createElement('p');
   boxCrochetText.setAttribute('id', 'boxCrochetText');
   boxCrochetText.innerHTML = 'Crochet';
   boxCrochet.appendChild(boxCrochetText);
   mainMenuOptions.appendChild(boxCrochet);
   containerMainMenu.appendChild(mainMenuOptions);
-  
   const footerMainMenu = document.createElement('footer');
   footerMainMenu.setAttribute('id', 'footerMainMenu');
   footerMainMenu.innerHTML = 'Derechos reservados @Tejer&Punto';
   containerMainMenu.appendChild(footerMainMenu);
-  
   return containerMainMenu;
 }
