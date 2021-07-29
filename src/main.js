b12bc553a11a35967fab11a3979133d41c164a9f
ni.colara@hotmail.com <ni.colara@hotmail.com>
2021-07-28 23:16:36 -0400

    se implementa borrar

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { firebaseConfig } from './lib/logic/firebConfig.js';
+ import { firebaseConfig } from './lib/logic/firebConfig.js';
 import { observer } from './lib/logic/observerFB.js';
 import { init } from './lib/router.js';
 
@@ -38,41 +38,71 @@ export const savePost = () => {
         tablePost.innerHTML='';
         querySnapshot.forEach((doc) => {
             console.log(`${doc.id} => ${doc.data().post}`);
+
+            const divPost= document.createElement('div');
+            divPost.setAttribute('class', 'divPost');
+
+            const textPost= document.createElement('div');
+            textPost.setAttribute('class', 'textPost');   
+            
+            const pPost= document.createElement('p');
+            pPost.innerHTML = ` ${doc.data().idUser} ${doc.data().post} ${doc.data().date}`;
+
+            const divButtons= document.createElement('div');
+            divButtons.setAttribute('id', 'buttons');               
+            
+            
+            const likeButton = document.createElement('button');
+            likeButton.setAttribute('id', 'likeButton');
+            likeButton.addEventListener('click', likePost(`${doc.id}`));
+
+            const dislikeButton = document.createElement('button');
+            dislikeButton.setAttribute('id', 'dislikeButton');
+            dislikeButton.addEventListener('click', likePost(`${doc.id}`));         
+            
+            const edit_button = document.createElement('button');
+            edit_button.setAttribute('id', 'edit-button');
+            edit_button.addEventListener('click', deletePost); 
+            edit_button.idPost = doc.id ;   //se asigna el id del post a una variable  para darselo a la funcion 
+
+
+            divButtons.appendChild(likeButton);
+            divButtons.appendChild(dislikeButton);
+            divButtons.appendChild(edit_button);
+
+            textPost.appendChild(pPost);
+
+            divPost.appendChild(divButtons);
+            divPost.appendChild(textPost);
+
+            tablePost.appendChild(divPost);
+            
+
+/*
         tablePost.innerHTML += `
-        <div class = "divPost">
+    <div class = "divPost">
         <div class = "textPost">
-       <p> ${doc.data().idUser} ${doc.data().post} ${doc.data().date} </p>
-       </div>
+            <p> ${doc.data().idUser} ${doc.data().post} ${doc.data().date} </p>
+        </div>
         <div id = "buttons">
-        <button id="likeButton" onclick= "likeButton( '${doc.id}')" ></button>
+            <button id="likeButton" onclick= "likeButton( '${doc.id}')" ></button>
+            
+            <button id="dislikeButton"  onclick= "likeButton( '${doc.id}')"></button>
+            <TextView id="textView" "> nlikes</textView>
+            
+            <button id="edit-button" class = "editButton" onclick= deletePost( '${doc.id}')"></button> 
         
-        <button id="dislikeButton"  onclick= "likeButton( '${doc.id}')"></button>
-        <TextView id="textView" "> nlikes</textView>
-        
-        <button id="edit-button" class = "editButton" onclick= deletePost( '${doc.id}')"></button> 
-       
-       </div>
+        </div>
 
-       </div>
-
-            `;   
+    </div>
+            `;
+            
+            */
         });
     });
 }
 
-  
-
-  
-  
-  
- 
-
-
-
-
-  
-
-const likePost = (idPost) => {
+export const likePost = (idPost) => {
    // FirebaseDatabase database = FirebaseDatabase.getInstance();
    
     console.log(idPost);
@@ -81,45 +111,16 @@ const likePost = (idPost) => {
 
 //const deleteButtonfunction=document.querySelector('#delete-button');
 //deleteButtonfunction.addEventListener('click', deletePost('${doc.id}'), 'false');
-export const deletePost = () => {
-  const deletePost= document.getElementById('readpost').value; // aqui se  obtiene  el valor del  texto que  hizo el usuario en el murp
-    const idUser = firebase.auth().currentUser;// obtener  el id del usuario que se encuentra usando firebase
-    console.log(idUser);
-    idUser: idUser.displayName,
-console.log(id );
-    db.collection('id').doc('${doc.id}').delete('${doc.id}').then(() => {
-      
+ 
+export const deletePost = (evt) => {
+    console.log(evt.currentTarget.idPost); // se lee la id del elemento a eliminar 
+    
+    db.collection('post').doc(evt.currentTarget.idPost).delete().then(() => {  // se pone la id del post a eliminar
         console.log("Document successfully deleted!");
-        // esto es para limpiar el campo donde se escriben los post
-            
+        readPost();// se pone readpost para refrescar la vista
     }).catch((error) => {
           console.error("Error removing document: ", error);
         });
-        readPost();
+
         
  }
- /*const Post = () => {
-
-  const divPost = document.createElement('div');
-  divPost.setAttribute('id', 'containerFeed');
-
-  const textPost=document.createElement('div');
-  textPost.setAttribute('id', 'generalContainer');
-  const Parrafo= document.createElement('p');
-  Parrafo.innerHTML= '${doc.data().idUser} ${doc.data().post} ${doc.data().date}';
-  parrafo.setAttribute('id', 'extraDiv');
-  textPost.appendChild(parrafo);
-  divPost.appendChild(textPost);
-
-
-
-  const buttonTest = document.createElement('button');
-  buttonTest.innerHTML = 'borrar';
-  buttonTest.setAttribute('id', 'deletebutton');
-  buttonTest.addEventListener('click', deletePost);
-  divPost.appendChild(buttonTest);
-
-  
-  
-  return divPost ;
-}*/
