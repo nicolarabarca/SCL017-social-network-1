bd8f75e07b6c0c80efb662487aa4e4c498952d55
carlamorales <carlamoralespacheco@gmail.com>
2021-08-04 10:02:45 -0400

    se arregla una ruta

--- src/lib/logic/signInLogic.js
+++ src/lib/logic/signInLogic.js
@@ -10,5 +10,6 @@ export const loginUser = () => {
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
+      window.location.href = '#/intro';
     });
 };
